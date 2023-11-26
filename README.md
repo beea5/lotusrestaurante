da3922a23f7741c05286123cf0986b77252497af
beea5 <142271226+beea5@users.noreply.github.com>
2023-11-26 15:32:01 -0300

    add crud final reserva

---
+++ README.md
@@ -0,0 +1,27 @@
+# Backend Lotus 
+
+Este repositório consiste no desenvolvimento do backend para o sistema de reservas de mesas do Lotus Restaurante. Utilizando tecnologias como Java, Spring Boot e Maven, o backend gerencia a criação e atualização de reservas.
+
+## Tecnologias
+
+- Java
+- Spring Boot
+- Thymeleaf (para renderização de views, se aplicável)
+- Maven
+- Banco de dados (não especificado)
+## Configuração do Ambiente
+
+Descreva os passos para configurar o ambiente de desenvolvimento.
+
+1. Instale o Java.
+2. Configure as variáveis de ambiente.
+3. Instale o Spring Boot.
+
+
+## Instalação
+
+
+```bash
+git clone https://github.com/seu-usuario/seu-projeto.git
+cd seu-projeto
+mvn install
--- lotusrestaurante/src/main/java/com/lotusrestaurante/lotusrestaurante/service/ReservasService.java
+++ lotusrestaurante/src/main/java/com/lotusrestaurante/lotusrestaurante/service/ReservasService.java
@@ -5,7 +5,6 @@ import com.lotusrestaurante.lotusrestaurante.repository.ReservasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,19 +17,27 @@ public class ReservasService {
         this.reservasRepository = reservasRepository;
     }
 
-    public List<ReservasEntity> getAllReservas() {
-        return reservasRepository.findAll();
-    }
-
-    public Optional<ReservasEntity> getReservaById(Long id) {
-        return reservasRepository.findById(id);
-    }
-
-    public ReservasEntity saveReserva(ReservasEntity reserva) {
+    public ReservasEntity  salvarReserva(ReservasEntity reserva) {
         return reservasRepository.save(reserva);
     }
 
-    public void deleteReserva(Long id) {
+    public Optional<ReservasEntity> findById(Long id) {
+        Optional<ReservasEntity> reservaExistente = reservasRepository.findById(id);
+
+        return reservaExistente;
+    }
+
+    public ReservasEntity save(ReservasEntity reservaExistente) {
+        return reservasRepository.save(reservaExistente);
+    }
+
+    public boolean existsById(Long id) {
+        return reservasRepository.existsById(id);
+    }
+
+    public void deleteById(Long id) {
         reservasRepository.deleteById(id);
     }
+
+    // Adicione métodos adicionais conforme necessário para buscar, atualizar, excluir reservas, etc.
 }
